3e4f238462a9def44701a8ee1d560b4a35e4b321
cnbttv <klcok@qq.com>
2025-12-16 10:14:48 +0800

    fix: Add missing Android build configuration

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -4,6 +4,24 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
